b9f9e0936c03acfebfc862f1573614d96dcff6ca
skarnov <obi.prog@gmail.com>
2023-01-05 20:37:38 +0600

    Bug Solved

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,14 @@
+import Alert from 'react-bootstrap/Alert';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import FooterNav from '../components/FooterNav';
 import NavBar from '../components/NavBar';
+import AuthUser from '../components/AuthUser';
 
 function Dashboard() {
+    const { user } = AuthUser();
+
     return (
         <>
             <Container fluid>
@@ -13,6 +17,9 @@ function Dashboard() {
                     <Col sm={12}>
                         <h3 className='mt-4'>Dashboard</h3>
                         <hr />
+                        <Alert variant="primary">
+                            <Alert.Heading>Welcome! {user.user_name}</Alert.Heading>
+                        </Alert>
                     </Col>
                 </Row>
                 <FooterNav />
