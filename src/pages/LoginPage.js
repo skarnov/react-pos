3893a1a847ff8262b12bac238bbd61b553f7a62a
skarnov <obi.prog@gmail.com>
2025-02-06 12:23:33 +0000

    Beta Testing Start

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -35,7 +35,7 @@ const LoginPage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="w-full max-w-md bg-white p-6 rounded-lg shadow-md">
-        <h2 className="text-2xl font-bold mb-4 text-gray-700 text-center">Login</h2>
+        <h2 className="text-2xl font-bold mb-4 text-gray-700 text-center">React POS Login</h2>
         {error && <p className="text-red-500 text-center mb-4">{error}</p>}
         <form onSubmit={handleLogin}>
           <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} className="w-full p-3 border border-gray-300 rounded-lg mb-4" required />
