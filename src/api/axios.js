e3fc45844757d21c5033daa90e858dae4fbaeab3
skarnov <you@example.com>
2025-08-05 02:44:21 +0100

    Ready To Test

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_LOCAL_URL,
+  baseURL: process.env.REACT_APP_PRODUCTION_URL,
   withCredentials: true,
 });
 
